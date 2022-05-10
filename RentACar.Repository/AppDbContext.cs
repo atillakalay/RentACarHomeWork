39b19408d5130dbe39e0e70362a343dcfdb92585
Atilla Kalay <atilla.kalayy@gmail.com>
2022-05-10 13:28:10 +0300

    AppDbContext Created

---
+++ RentACar.Repository/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentACar.Core;
+
+namespace RentACar.Repository
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+    }
+}
